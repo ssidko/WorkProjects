515bb6a2e5098d89b72ec78884d8f1809847e091
ssidko <troyvova@gmail.com>
2013-07-22 20:26:09 +0300

    arm

--- test/main.c
+++ test/main.c
@@ -4,23 +4,55 @@
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_rcc.h"
 
+#define VECT_TAB_OFFSET  0x00
+
+#define RCC_PLLCFGR_M						((uint32_t)0x00000008)
+#define RCC_PLLCFGR_N						((uint32_t)0x00000150) // 336
+#define RCC_PLLCFGR_P						((uint32_t)0x00000002)
+#define RCC_PLLCFGR_Q						((uint32_t)0x00000007)
+
 void SystemInitialization(void)
 {
-	// Подключаем HSE (8 MHz).
+	// Подключаем HSE (8 MHz) и ожидаем его стабилизации.
 	RCC->CR |= RCC_CR_HSEON;
+	while ((RCC->CR & RCC_CR_HSERDY) == 0x00) {}
 
-	// Ожидаем пока стабилизируется HSE.
-	while (RCC->CR & RCC_CR_HSERDY) {}
+	// Reset RCC->CFGR
+	RCC->CFGR = 0x00000000;
 
-	// В качестве источника частоты system clock (SYSCLK) выбираем HSE (8 MHz).
-	RCC->CFGR |= RCC_CFGR_SW_HSE;
+	// SYSCLK not divided. AHB = 168 MHz.
+	RCC->CFGR |= RCC_CFGR_HPRE_DIV1;
 
+	// HCLK divided by 2. APB2 = 84 MHz
+	RCC->CFGR |= RCC_CFGR_PPRE2_DIV2;
+
+	// HCLK divided by 4. APB1 = 42 MHz
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV4;
+
+	// Select regulator voltage output Scale 1 mode, System frequency up to 168 MHz
+	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
+	PWR->CR |= PWR_CR_VOS;
+
+	// Конфигурирование PLL
+	RCC->PLLCFGR = RCC_PLLCFGR_M | (RCC_PLLCFGR_N << 6) | (RCC_PLLCFGR_P << 16) | RCC_PLLCFGR_PLLSRC_HSE | (RCC_PLLCFGR_Q << 24) ;
+
+	// Активируем PLL и ожидаем его готовности.
+	RCC->CR |= RCC_CR_PLLON;
+	while ((RCC->CR & RCC_CR_PLLRDY) == 0x00) {}
+
+    /* Configure Flash prefetch, Instruction cache, Data cache and wait state */
+    FLASH->ACR = FLASH_ACR_ICEN |FLASH_ACR_DCEN |FLASH_ACR_LATENCY_5WS;
+
+	// В качестве источника частоты system clock (SYSCLK) выбираем PLL (168 MHz).
+	RCC->CFGR |= RCC_CFGR_SW_PLL;
 }
 
 int main(void)
 {
 
-	SystemInit();
+	//SystemInit();
+
+	SystemInitialization();
 
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
 
