2801377575712f42fb07bc04d0c8606bbcd6e5af
ssidko <troyvova@gmail.com>
2015-05-06 23:52:23 +0300

    File

--- Zfs/MyLib/File.cpp
+++ Zfs/MyLib/File.cpp
@@ -1,18 +1,17 @@
 #include "File.h"
 
-File::File(const String &_file_name, FileMode _mode, const ULONGLONG &offset_in_bytes) :
+File::File(const String &_file_name, FileMode _mode) :
 	handle(INVALID_HANDLE_VALUE),
 	file_name(_file_name),
 	mode(_mode)
 {
 }
 
-
 File::~File()
 {
 }
 
-DWORD GetDeisiredAccess(FileMode open_mode)
+DWORD File::GetDesiredAccess(FileMode open_mode)
 {
 	DWORD desired_access = 0;
 
@@ -27,17 +26,66 @@ DWORD GetDeisiredAccess(FileMode open_mode)
 	return desired_access;
 }
 
+DWORD File::GetFlagsAndAttributes(FileMode open_mode)
+{
+	DWORD flags_attributes = FILE_ATTRIBUTE_NORMAL;
+
+	if (open_mode && FileMode::kNoBuffering) {
+		flags_attributes |= FILE_FLAG_NO_BUFFERING;
+	}
+
+	return flags_attributes;
+}
+
 BOOL File::Open(void)
 {
 	DWORD desired_access = GetDesiredAccess(mode);
+	DWORD flags_attributes = GetFlagsAndAttributes(mode);
 
-	handle = ::CreateFile(file_name.c_str(),
-		desired_access,
-		FILE_SHARE_READ | FILE_SHARE_WRITE,
-		NULL,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		NULL);
-
+	if (desired_access != -1) {
+		handle = ::CreateFile(file_name.c_str(),					// lpFileName 
+								desired_access,						// dwDesiredAccess 
+								FILE_SHARE_READ | FILE_SHARE_WRITE,	// dwShareMode 
+								NULL,								// lpSecurityAttributes 
+								OPEN_EXISTING,						// dwCreationDisposition 
+								flags_attributes,					// dwFlagsAndAttributes 
+								NULL);								// hTemplateFile 
+		if (handle != INVALID_HANDLE_VALUE) {			
+			return TRUE;
+		}
+	}
 	return FALSE;
+}
+
+BOOL File::Create(void)
+{
+	DWORD desired_access = GetDesiredAccess(mode);
+	DWORD flags_attributes = GetFlagsAndAttributes(mode);
+
+	if (desired_access != -1) {
+		handle = ::CreateFile(file_name.c_str(),					// lpFileName 
+								desired_access,						// dwDesiredAccess 
+								FILE_SHARE_READ | FILE_SHARE_WRITE,	// dwShareMode 
+								NULL,								// lpSecurityAttributes 
+								CREATE_ALWAYS,						// dwCreationDisposition 
+								flags_attributes,					// dwFlagsAndAttributes 
+								NULL);								// hTemplateFile 
+		if (handle != INVALID_HANDLE_VALUE) {
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+void File::Close(void)
+{
+	::CloseHandle(handle);
+	handle = INVALID_HANDLE_VALUE;
+}
+
+BOOL File::SetPointer(const ULONGLONG &offset)
+{
+	LARGE_INTEGER new_offset;
+	new_offset.QuadPart = (LONGLONG)offset;
+	return ::SetFilePointerEx(handle, new_offset, NULL, FILE_BEGIN);
 }
--- Zfs/MyLib/File.h
+++ Zfs/MyLib/File.h
@@ -12,13 +12,14 @@ public:
 	virtual BOOL Create(void) = 0;
 	virtual void Close(void) = 0;
 	virtual BOOL SetPointer(const ULONGLONG &offset) = 0;
-	virtual BOOL Read(const BYTE *buffer, DWORD size_in_bytes) = 0;
-	virtual BOOL Write(const BYTE *buffer, DWORD size_in_bytes) = 0;
+	virtual DWORD Read(const BYTE *buffer, DWORD size_in_bytes) = 0;
+	virtual DWORD Write(const BYTE *buffer, DWORD size_in_bytes) = 0;
 };
 
 enum FileMode {
-	kReadOnly = 1 << 1,
-	kReadWrite = 1 << 2,
+	kReadOnly		= 1 << 1,
+	kReadWrite		= 1 << 2,
+	kNoBuffering	= 1 << 3, 
 };
 
 class File : public IFile
@@ -28,16 +29,17 @@ private:
 	String file_name;
 	FileMode mode;
 	DWORD GetDesiredAccess(FileMode open_mode);
+	DWORD GetFlagsAndAttributes(FileMode open_mode);
 public:
-	File(const String &_file_name, FileMode _mode, const ULONGLONG &offset_in_bytes = 0);
+	File(const String &_file_name, FileMode _mode);
 	~File();
 
 	virtual BOOL Open(void);
 	virtual BOOL Create(void);
 	virtual void Close(void);
 	virtual BOOL SetPointer(const ULONGLONG &offset);
-	virtual BOOL Read(const BYTE *buffer, DWORD size_in_bytes);
-	virtual BOOL Write(const BYTE *buffer, DWORD size_in_bytes);
+	virtual DWORD Read(const BYTE *buffer, DWORD size_in_bytes);
+	virtual DWORD Write(const BYTE *buffer, DWORD size_in_bytes);
 };
 
 #endif // _FILE_H
