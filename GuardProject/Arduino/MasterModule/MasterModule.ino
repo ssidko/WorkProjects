99b9754f68f87ab021083aacfa661b051ead1650
ssidko <troyvova@gmail.com>
2015-10-08 02:04:05 +0300

    Add MasterModule

---
+++ GuardProject/Arduino/MasterModule/MasterModule.ino
@@ -0,0 +1,46 @@
+#define LED_PIN                 13
+
+#define GUARD_SENSOR_1_PIN      12
+#define KEY_1_PIN               11
+
+#define GSM_LINE_1_PIN          10
+#define GSM_LINE_2_PIN          9
+#define GSM_LINE_3_PIN          8
+#define GSM_LINE_4_PIN          7
+#define GSM_LINE_5_PIN          6
+
+#define CONTROL_LINE_1_PIN      2
+#define CONTROL_LINE_2_PIN      3
+
+void setup() {
+  pinMode(GUARD_SENSOR_1_PIN, INPUT);
+  pinMode(KEY_1_PIN, INPUT);
+  pinMode(GSM_LINE_1_PIN, INPUT);
+  pinMode(GSM_LINE_2_PIN, INPUT);
+  pinMode(GSM_LINE_3_PIN, INPUT);
+  pinMode(GSM_LINE_4_PIN, INPUT);
+  pinMode(GSM_LINE_5_PIN, INPUT);
+  pinMode(CONTROL_LINE_1_PIN, OUTPUT);
+  pinMode(CONTROL_LINE_2_PIN, OUTPUT);
+  digitalWrite(CONTROL_LINE_1_PIN, HIGH);
+  digitalWrite(CONTROL_LINE_2_PIN, HIGH);
+}
+
+void loop() {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- GuardProject/Arduino/SlaveModule/SlaveModule.ino
+++ GuardProject/Arduino/SlaveModule/SlaveModule.ino
@@ -1,48 +1,47 @@
-#define LED_PIN    13
+#define LED_PIN                 13
 
-#define SENSOR_1_PIN          10
-#define SENSOR_2_PIN          11
-#define SENSOR_3_PIN          12
+#define GUARD_SENSOR_1_PIN      12
 
-#define CONTROL_LINE_1_PIN    2
-#define CONTROL_LINE_2_PIN    3
-#define CONTROL_LINE_3_PIN    4
+#define CONTROL_LINE_3_PIN      2
+#define CONTROL_LINE_4_PIN      3
+#define CONTROL_LINE_5_PIN      4
 
 void setup() {
-  Serial.begin(9600);
-  
-  pinMode(SENSOR_1_PIN, INPUT);
-  pinMode(SENSOR_2_PIN, INPUT);
-  pinMode(SENSOR_3_PIN, INPUT);
-  
-  pinMode(CONTROL_LINE_1_PIN, OUTPUT);
-  digitalWrite(CONTROL_LINE_1_PIN, HIGH);
-  pinMode(CONTROL_LINE_2_PIN, OUTPUT);
-  digitalWrite(CONTROL_LINE_2_PIN, HIGH);
+  Serial.begin(9600);  
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(GUARD_SENSOR_1_PIN, INPUT);
   pinMode(CONTROL_LINE_3_PIN, OUTPUT);
+  pinMode(CONTROL_LINE_4_PIN, OUTPUT);
+  pinMode(CONTROL_LINE_5_PIN, OUTPUT);
   digitalWrite(CONTROL_LINE_3_PIN, HIGH);
+  digitalWrite(CONTROL_LINE_4_PIN, HIGH);
+  digitalWrite(CONTROL_LINE_5_PIN, HIGH);
+}
+
+void Blink(int msec, int times)
+{
+  digitalWrite(LED_PIN, LOW);
+  for(int i = 0; i < times; i++) {
+    digitalWrite(LED_PIN, HIGH);
+    delay(msec);
+    digitalWrite(LED_PIN, LOW);
+    delay(msec);
+  }
 }
 
 void MakeSingleImpulse(int pin)
 {
   digitalWrite(pin, LOW);
-  delay(1000);
+  delay(500);
   digitalWrite(pin, HIGH);
-  //delay(1000);
 }
 
 void loop() {
-
-  if (digitalRead(SENSOR_1_PIN) == HIGH) {
-      MakeSingleImpulse(CONTROL_LINE_1_PIN);
-  }  
-  if (digitalRead(SENSOR_2_PIN) == HIGH) {
-      MakeSingleImpulse(CONTROL_LINE_2_PIN);
-  }
-  if (digitalRead(SENSOR_3_PIN) == HIGH) {
+  if (digitalRead(GUARD_SENSOR_1_PIN) == LOW) {
       MakeSingleImpulse(CONTROL_LINE_3_PIN);
+      MakeSingleImpulse(CONTROL_LINE_4_PIN);
+      MakeSingleImpulse(CONTROL_LINE_5_PIN);
   }
-  
 }
 
 
