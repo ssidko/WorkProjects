1371942e90c262f8cd165dee66b98ed4c37f5dff
ssidko <troyvova@gmail.com>
2016-05-18 23:38:21 +0300

    ++

--- GuardProject/WinService/WinService/WinService.cpp
+++ GuardProject/WinService/WinService/WinService.cpp
@@ -3,8 +3,15 @@
 #include "Service.h"
 #include "SvcGeneral.h"
 
+void Test(void)
+{
+	ServiceRun();
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+	//Test();
+
 	SERVICE_TABLE_ENTRY dispatch_table[] = {
 		{SERVICE_NAME, ServiceMain},
 		{NULL, NULL}
