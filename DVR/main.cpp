02bf82e2b885f15c573758a7e8a36091ae658e89
ssidko <troyvova@gmail.com>
2016-11-11 18:52:48 +0200

    ++

--- DVR/main.cpp
+++ DVR/main.cpp
@@ -109,8 +109,8 @@ int main(int argc, char *argv[])
 
 	//DHFS::StartRecovering("\\\\.\\PhysicalDrive0", "e:\\40175\\out\\", dvr::Timestamp(2015, 1, 1, 0, 0, 0), dvr::Timestamp(0, 0, 0, 0, 0, 0));
 
-	DHFS::StartRecovering("\\\\.\\PhysicalDrive1", "F:\\40773\\out\\", dvr::Timestamp(2015, 5, 1, 0, 0, 0), dvr::Timestamp(0, 0, 0, 0, 0, 0));
-	//DHFS::Volume volume("\\\\.\\PhysicalDrive1");
+	DHFS::StartRecovering("\\\\.\\PhysicalDrive0", "F:\\40773\\out\\", dvr::Timestamp(2015, 5, 1, 0, 0, 0), dvr::Timestamp(0, 0, 0, 0, 0, 0));
+	//DHFS::Volume volume("\\\\.\\PhysicalDrive0");
 	//if (volume.Open()) {
 	//	volume.SaveFrameInfo("F:\\40773\\frames.txt");
 	//	//volume.SaveFrameSequenceInfo("F:\\39988\\sequences.txt");
