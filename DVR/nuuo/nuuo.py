2ea27b1fa5febd1e5ddc87779cabbf93687a971f
ssidko <troyvova@gmail.com>
2017-03-20 18:58:03 +0200

    nuuo

--- DVR/nuuo/nuuo.py
+++ DVR/nuuo/nuuo.py
@@ -1,6 +1,8 @@
 import os
 import struct
 import time
+from collections import namedtuple
+
 
 FFMPEG_APP = r"D:\Soft\ffmpeg\bin\ffmpeg.exe"
 MKVMERGE_APP = r" D:\Soft\#RecoverySoft#\mkvtoolnix\mkvmerge.exe"
@@ -11,11 +13,17 @@ CMD_LINE_FORMAT = FFMPEG_CMD_FORMAT
 
 file_type  = {'h264': b'H264', 'mjpeg': b'NUMJ'}
 
+FRAME_HEADER_SIZE = 12
+
+FrameHeader = namedtuple('FrameHeader', ['sign', 'flags', 'unk', 'size'])
+
+
 def NormalizeDirPath(path):
     path = path.rstrip()
     if path[len(path) - 1] != "\\":
         path += "\\"
     return path
+
 
 def FileType(path):
     with open(path, 'rb') as file:
@@ -25,36 +33,79 @@ def FileType(path):
                 return t
     return "unknown"
 
-def run():
 
-    input_file = r'F:\Root\DATA 3\20161226\C00017\C00017S00A20161226213405164.dat    '
-    input_file.rstrip()
-    avi_file = r"F:\out.avi"
+def ReadFrameHeader(file):
+    buffer = file.read(FRAME_HEADER_SIZE)
+    if len(buffer) == FRAME_HEADER_SIZE:
+        return FrameHeader._make(struct.unpack('<HHII', buffer))
 
-    file_path ="F:\\Root\\DATA 3\\20161226\\C00017\\C00017S00A20161226234608231.dat"
-    info = os.stat(file_path)
-    print("file size: ", info.st_size)
-    print('file type: ', FileType(file_path))
 
-    with open(file_path, "rb") as file:
+def IsValidFileStructure(path):
+    try:
+        type = FileType(path)
+        if type != "unknown":
+            with open(path, "rb") as file:
+                # Skip file header
+                file.seek(64)
+                frame_header = ReadFrameHeader(file)
+                while frame_header:
+                    if frame_header.sign == 0x01ff:
+                        #print('Frame at: ', str(file.tell() - FRAME_HEADER_SIZE))
+                        file.seek(frame_header.size, 1)
+                        frame_header = ReadFrameHeader(file)
+                        continue
+                    if frame_header.sign == 0xffff:
+                        file.seek(-FRAME_HEADER_SIZE, 1)
+                        footer = file.read(12)
+                        if footer == b'\xFF\xFF\xFF\xFF\x01\x00\xEE\xFF\x10\x00\x00\x00':
+                            #print('Footer at: ', str(file.tell() - FRAME_HEADER_SIZE))
+                            return True
+                    return False
 
-        header_fmt = '<H'
+    except BaseException as exc:
+        print("Error with file: ", path)
+        print(exc)
 
-        file.seek(64)
-        raw = file.read(12)
+    return False
 
-        signature,flags,val1,size = struct.unpack('<HHII', raw)
-        print('signature :', signature)
-        print('flags :', flags)
-        print('size :', size)
+def ValidateAllFiles(path):
+    for file_name in os.listdir(path):
+        file_path = path + file_name
+        if os.path.isdir(file_path):
+            file_path = NormalizeDirPath(file_path)
+            ValidateAllFiles(file_path)
+        else:
+            if IsValidFileStructure(file_path):
+                pass
+            else:
+                info = os.stat(file_path)
+                print("File: %s; Size: %d bytes; Type: %s; Integrity: CORRUPTED" % (file_path, info.st_size, FileType(file_path)))
+            pass
+    pass
 
-        pass
+def run(src_dir, dst_dir):
+
+    if not os.path.exists(src_dir):
+        print("Source directory (%s) not exist" % (src_dir))
+        return
+
+    if not os.path.exists(dst_dir):
+        print('Destionation directory not exist')
+        print('Create destionation directory: "%s"' % (dst_dir))
+        os.mkdir(dst_dir)
+        if os.path.exists(dst_dir):
+            print('Destination directory created')
+        else:
+            print("Could not create destination directory")
+            return
+
+    src_dir = "E:\\41631"
+
+    src_dir = NormalizeDirPath(src_dir)
+
+    #ValidateAllFiles(src_dir)
 
     #print(CMD_LINE_FORMAT.format(ffmpeg=APP, in_file=input_file, out_file=avi_file))
     #os.system(CMD_LINE_FORMAT.format(ffmpeg=APP, in_file=input_file, out_file=avi_file))
 
-
-
-    pass
-
-
+    return
