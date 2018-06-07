739e30733d434d9b41026fcf048a19928746a2d0
ssidko <troyvova@gmail.com>
2018-06-07 18:57:04 +0300

    ++

--- DVR/main.cpp
+++ DVR/main.cpp
@@ -112,109 +112,10 @@ void ToHexString(uint8_t *buff, size_t count, std::string &str)
 #include "1CDfile.h"
 #include "ExtentRecovery.h"
 
-#include <filesystem>
-
-
-namespace fs = std::experimental::filesystem;
-
-void ttt()
-{
-	using namespace ext4;
-
-	std::map<uint32_t, std::string> files;
-	fs::path extent_dir("F:\\44326\\extents");
-	for (auto &it : fs::directory_iterator(extent_dir)) {
-		std::string file_name = it.path().filename().string();
-		uint32_t block = std::stoi(file_name);
-		files[block] = it.path().string();
-	}
-
-	uint32_t block = 1861255175;
-	fs::path out_dir("F:\\44326\\file");
-	fs::create_directories(out_dir);
-
-	auto it = files.find(block);
-	if (it != files.end()) {
-
-		W32Lib::FileEx io(it->second.c_str());
-		if (io.Open()) {
-			
-			std::vector<uint8_t> buff(io.GetSize());
-			EXTENT_BLOCK *extent_block = (EXTENT_BLOCK *)&buff[0];
-
-			io.Read(&buff[0], buff.size());
-			io.Close();
-
-			assert(extent_block->header.depth);
-
-			fs::rename(it->second, out_dir/fs::path(it->second).filename());
-
-			for (int i = 0; i < extent_block->header.entries; i++) {
-				auto itt = files.find((uint32_t)extent_block->extent_index[i].PysicalBlock());
-				if (itt != files.end()) {
-					fs::rename(itt->second, out_dir / fs::path(itt->second).filename());
-				}
-			}
-		
-		}	
-	}
-
-}
-
-bool SaveAllBlocksWithExtents(const std::string &io_file_name, const std::string &out_dir_path)
-{
-	using namespace ext4;
-
-	const size_t block_size = 4096;
-	const size_t blocks_count = 2196904704;
-	const size_t extents_per_block = (block_size - sizeof(EXTENT))/sizeof(EXTENT);
-
-	W32Lib::FileEx io(io_file_name.c_str());
-	if(!io.Open()) {
-		return false;
-	}
-
-	size_t block = 0;
-	std::vector<uint8_t> buff(block_size);
-	char str_buff[255] = { 0 };
-
-	for (size_t block = 0; block < blocks_count; block++) {
-			
-		io.SetPointer(block * block_size);
-		io.Read(&buff[0], block_size);
-
-		EXTENT_BLOCK *extent_block = (EXTENT_BLOCK *)&buff[0];
-
-		if (extent_block->header.magic == EXTENT_HEADER_MAGIC) {
-			if (extent_block->header.max == extents_per_block) {
-				if (extent_block->header.entries) {
-				
-					std::sprintf(str_buff, "%010lld level=%d", block, extent_block->header.depth);
-					std::string file_name(str_buff);
-					std::string out_file_path = out_dir_path + file_name;
-
-					W32Lib::FileEx out_file(out_file_path.c_str());
-					if (!out_file.Create()) {
-						throw std::exception();
-					}
-
-					out_file.Write(buff.data(), (extent_block->header.entries + 1) * sizeof(EXTENT));
-				
-				}
-			}
-		}	
-	}
-	return true;
-}
-
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	MainWindow w;
-
-	//SaveAllBlocksWithExtents("F:\\44326\\raid5.img", "F:\\44326\\extents\\");
-
-	ttt();
 
 	using namespace db1cd;
 
