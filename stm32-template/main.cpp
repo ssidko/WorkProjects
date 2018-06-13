922ac4b9e166baca103f78a04dd7789cc5d9f7a9
ssidko <troyvova@gmail.com>
2018-06-13 11:10:39 +0300

    add stm32-template

---
+++ stm32-template/main.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+
+#include <stdint.h>
+#include "stm32f1xx.h"
+
+
+extern "C" int main();
+
+/*
+extern "C" void SystemInit(void)
+{
+    main();
+}
+*/
+
+extern "C" int main()
+{
+    //cout << "hello!";
+    
+    int x = 0;
+
+    while (true) {
+
+        x++;
+
+
+    }
+
+    return 0;
+}
